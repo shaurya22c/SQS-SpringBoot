3fec8f31344d9b17d69ef5f538a4506e3f2d7917
shaurya22c <shauryabavishi2016@gmail.com>
2021-01-18 14:40:01 -0600

    Sqs-SpringBoot

---
+++ src/main/java/com/sqsproject/controller/SQSController.java
@@ -0,0 +1,30 @@
+package com.sqsproject.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SQSController {
+
+    @Autowired
+    private QueueMessagingTemplate queueMessagingTemplate;
+
+    @Value("${cloud.aws.end-point.uri}")
+    private String endPoint;
+
+    @GetMapping("/put/{msg}")
+    public void putMessagesToQueue(@PathVariable("msg") String message){
+        queueMessagingTemplate.send(endPoint, MessageBuilder.withPayload(message).build());
+    }
+
+    @SqsListener("sqs-queue")
+    public void loadMessagesQueue(String message){
+        System.out.println("Queue messages: "+message);
+    }
+}
---
+++ src/test/java/com/sqsproject/SqsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
